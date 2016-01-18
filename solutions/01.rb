2e87c65cd59990e2442934ceed05889051edb1cb
hrist-todorova <hrist_todorova@abv.bg>
2016-01-18 21:01:33 +0200

    Revert "make smaller code"
    This reverts commit eef53753f49f623bfeef9f3aea93b4f271be1350.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,10 +1,22 @@
 def convert_to_bgn(price, currency)
-  exchange = {usd: 1.7408, eur: 1.9557, gbp: 2.6415, bgn: 1 }
-  (price * exchange[currency]).round(2)
+  case
+  when currency == :usd
+    result = price * 1.7408
+
+  when currency == :eur
+    result = price * 1.9557
+
+  when currency == :gbp
+    result = price * 2.6415
+
+  else
+    result = price
+
+  end
+  result.round(2)
 end
 
-def compare_prices(first_price, first_currency,
-                   second_price, second_currency)
-  convert_to_bgn(first_price, first_currency) <=>
+def compare_prices(first_price, first_currency, second_price, second_currency)
+  convert_to_bgn(first_price, first_currency) -
   convert_to_bgn(second_price, second_currency)
 end
