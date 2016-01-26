d97c88fb815987e885b3ce8ea3f3a7fc2d0bb082
hrist-todorova <hrist_todorova@abv.bg>
2016-01-26 18:07:42 +0200

    changed original solution a lot

--- solutions/04.rb
+++ solutions/04.rb
@@ -1,23 +1,17 @@
 class Card
+
+  attr_accessor :rank, :suit
+
   def initialize(rank, suit)
-    @rank = rank
-    @suit = suit
+    @rank, @suit = rank, suit
   end
 
   def rank
     @rank
   end
 
-  def suit
-    @suit
-  end
-
   def to_s
-    suits = {spades: "Spades", hearts: "Hearts",
-            diamonds: "Diamonds", clubs: "Clubs"}
-    ranks = {2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9,
-            10 => 10, jack: "Jack", queen: "Queen", king: "King", ace: "Ace"}
-    "#{ranks[@rank]} of #{suits[@suit]}"
+    @rank.to_s.capitalize + " of " + @suit.to_s.capitalize
   end
 
   def ==(other)
@@ -29,50 +23,54 @@ end
 class Deck
   include Enumerable
 
-  def initialize(deck = nil)
-    @deck = deck
-    @ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
-    @sorted_deck
-    if deck == nil
-      @deck = []
-      for i in 0...@ranks.length
-      @deck << Card.new(@ranks[i], :spades) << Card.new(@ranks[i], :hearts)
-      @deck << Card.new(@ranks[i], :diamonds) << Card.new(@ranks[i], :clubs)
-      end
+  def initialize(cards = nil)
+    @ranks = [:ace, :king, :queen, :jack, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+    @suits = [:spades, :hearts, :diamonds, :clubs]
+
+    if cards == nil
+      @cards = generate_deck
+    else
+      @cards = cards.dup
     end
+
+  end
+
+  def each
+    @cards.each { |card| yield card }
+  end
+
+  def generate_deck
+    product = @ranks.product @suits
+    product.map { |card| Card.new *card }
   end
 
   def size
-    @deck.length
+    self.to_a.size
   end
 
   def draw_top_card
-    @deck.shift
+    @cards.shift
   end
 
   def draw_bottom_card
-    @deck.pop
+    @cards.pop
   end
 
   def top_card
-    @deck[0]
+    @cards.first
   end
 
   def bottom_card
-    @deck[-1]
+    @cards.last
   end
 
   def shuffle
-    @deck.shuffle!
+    @cards.shuffle!
   end
 
   def sort
-    @sorted_deck = []
-    sort_by_suit(:clubs)
-    sort_by_suit(:diamonds)
-    sort_by_suit(:hearts)
-    sort_by_suit(:spades)
-    @deck = @sorted_deck.compact!
+    @cards.sort_by! { |card| [@suits.index(card.suit),
+                              @ranks.index(card.rank)] }
   end
 
   def deal
@@ -80,258 +78,183 @@ class Deck
   end
 
   def to_s
-    for i in 0...size
-      puts @deck[i].to_s
-    end
+    @cards.map(&:to_s).join("\n")
   end
 
-  private
-  def sort_by_suit(suit)
-    for j in 0...@ranks.length
-      if @deck.include?( Card.new(@ranks[j], suit) )
-         @sorted_deck.insert(1, Card.new(@ranks[j], suit))
+  class Hand
+
+      def initialize(cards)
+        @cards = cards.dup
       end
-    end
+
+      def size
+        @cards.size
+      end
+
   end
 
 end
 
 class WarDeck < Deck
-  def initialize(deck = nil)
-    @ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
-    @suits = [:spades, :hearts, :diamonds, :clubs]
-    if deck == nil
-      @deck = []
-      for i in 0...@ranks.length
-      @deck << Card.new(@ranks[i],@suits[0]) << Card.new(@ranks[i],@suits[1])
-      @deck << Card.new(@ranks[i],@suits[2]) << Card.new(@ranks[i],@suits[3])
-      end
-    else
-      @deck = deck
-    end
-  end
 
-  def each
-    for i in 0...size
-      yield @deck[i]
-    end
-  end
-
-  class Hand
-    def initialize(hand)
-      @data = hand
-    end
-
-    def size
-      @data.length
-    end
-
-    def play_card
-      @data.shift
-    end
-
-    def allow_face_up?
-      @data.length <= 3
-    end
+  def initialize(cards = nil)
+    @hand_size = 26
+    super cards
   end
 
   def deal
-    shuffle
-    hand = []
-    26.times do
-      if @deck.length != 0
-        hand << draw_top_card
-      end
+    hand = WarHand.new @cards[0...@hand_size]
+    @cards = @cards.drop(@hand_size)
+    hand
+  end
+
+  class WarHand < Hand
+
+    def initialize(cards)
+      super
     end
-    Hand.new(hand)
+
+    def play_card
+      @cards.shift
+    end
+
+    def allow_face_up?
+      @cards.size <= 3
+    end
+
   end
 
 end
 
 class BeloteDeck < Deck
-  def initialize(deck = nil)
-    @ranks = [7, 8, 9, :jack, :queen, :king, 10, :ace]
+
+  attr_reader :ranks
+
+  def initialize(cards = nil)
+    @hand_size = 8
+    @ranks = [:ace, :king, :queen, :jack, 10, 9, 8, 7]
     @suits = [:spades, :hearts, :diamonds, :clubs]
-    if deck == nil
-      @deck = []
-      for i in 0...@ranks.length
-      @deck << Card.new(@ranks[i],@suits[0]) << Card.new(@ranks[i],@suits[1])
-      @deck << Card.new(@ranks[i],@suits[2]) << Card.new(@ranks[i],@suits[3])
-      end
+
+    if cards == nil
+      @cards = generate_deck
     else
-      @deck = deck
+      @cards = cards.dup
     end
   end
 
-  def each
-    for i in 0...size
-      yield @deck[i]
-    end
+  def deal
+    hand = BeloteHand.new @cards[0...@hand_size]
+    @cards = @cards.drop @hand_size
+    hand
   end
 
-  class Hand
-    def initialize(hand)
-      @data = hand
-    end
+  class BeloteHand < Hand
 
-    def size
-      @data.length
-    end
-
-    def includes(a, b)
-      @data.include?(Card.new(a, b))
+    def initialize(cards)
+      super
     end
 
     def highest_of_suit(suit)
-      ranks = [7, 8, 9, :jack, :queen, :king, 10, :ace]
-      index = 0
-      for i in 0..7
-        if includes(ranks[- 1 - i], suit)
-          index = @data.index(Card.new(ranks[- 1 - i], suit))
-          break
-        end
-      end
-      @data[index]
+      deck = BeloteDeck.new @cards.select { |card| card.suit == suit }
+      deck.sort
+      deck.top_card
     end
 
     def belote?
-       (includes(:queen, :spades) and includes(:king, :spades)) or
-       (includes(:queen, :hearts) and includes(:king, :hearts)) or
-       (includes(:queen, :diamonds) and includes(:king, :diamonds)) or
-       (includes(:queen, :clubs) and includes(:king, :clubs))
-    end
-
-    def has_tierce?(suit)
-      @ranks = [7, 8, 9, :jack, :queen, :king, 10, :ace]
-      for i in 0...6
-        if includes(@ranks[i], suit) and includes(@ranks[i + 1], suit) and
-           includes(@ranks[i + 2], suit)
-          return true
-        end
-      end
-      false
+      @cards.select { |card| card.rank == :king or card.rank == :queen }
+      .group_by { |card| card.suit }
+      .any? { |_, same_suit| same_suit.size == 2 }
     end
 
     def tierce?
-      has_tierce?(:spades) or has_tierce?(:hearts) or
-       has_tierce?(:diamonds) or has_tierce?(:clubs)
-    end
-
-    def has_quarte?(suit)
-       @ranks = [7, 8, 9, :jack, :queen, :king, 10, :ace]
-       for i in 0...5
-          if includes(@ranks[i], suit) and includes(@ranks[i + 1], suit) and
-             includes(@ranks[i + 2], suit) and includes(@ranks[i + 3], suit)
-            return true
-          end
-       end
-       false
+      check_for_consecutive?(3)
     end
 
     def quarte?
-      has_quarte?(:spades) or has_quarte?(:hearts) or
-       has_quarte?(:diamonds) or has_quarte?(:clubs)
-    end
-
-    def has_quint?(suit)
-       @ranks = [7, 8, 9, :jack, :queen, :king, 10, :ace]
-       for i in 0...4
-          if includes(@ranks[i], suit) and includes(@ranks[i + 1], suit) and
-             includes(@ranks[i + 2], suit) and includes(@ranks[i + 3], suit) and
-             includes(@ranks[i + 4], suit)
-            return true
-          end
-       end
-       false
+      check_for_consecutive?(4)
     end
 
     def quint?
-      has_quint?(:spades) or has_quint?(:hearts) or
-       has_quint?(:diamonds) or has_quint?(:clubs)
+      check_for_consecutive?(5)
     end
 
     def carre_of_jacks?
-      includes(:jack, :spades) and includes(:jack, :hearts) and
-      includes(:jack, :diamonds) and includes(:jack, :clubs)
+      carre?(:jack)
     end
 
     def carre_of_nines?
-      includes(9, :spades) and includes(9, :hearts) and
-      includes(9, :diamonds) and includes(9, :clubs)
+      carre?(9)
     end
 
     def carre_of_aces?
-      includes(:ace, :spades)   and includes(:ace, :hearts) and
-      includes(:ace, :diamonds)and includes(:ace, :clubs)
+      carre?(:ace)
     end
 
- end
+    private
 
-  def deal
-    shuffle
-    hand = []
-    8.times do
-      if @deck.length != 0
-        hand << draw_top_card
+    def carre?(rank)
+      @cards.count { |card| card.rank == rank } == 4
+    end
+
+    def check_for_consecutive?(n)
+      groups = @cards.group_by { |card| card.suit }
+      check = groups.map { |g| consecutive? g[1], n }
+      check.any? { |predicate| predicate == true }
+    end
+
+    def consecutive?(cards, count)
+      cards = cards.map { |c| BeloteDeck.new.ranks.find_index(c.rank) }.sort
+      last = cards[0]
+      groups = cards.slice_before do |e|
+        last, prev = e, last
+        prev + 1 != e
       end
+      groups.to_a.any? { |group| group.size > 2 }
     end
-    Hand.new(hand)
+
   end
 
 end
 
 class SixtySixDeck < Deck
-  def initialize(deck = nil)
-    @ranks = [9, :jack, :queen, :king, 10, :ace]
+
+  def initialize(cards = nil)
+    @hand_size = 6
+    @ranks = [:ace, :king, :queen, :jack, 10, 9]
     @suits = [:spades, :hearts, :diamonds, :clubs]
-    if deck == nil
-      @deck = []
-      for i in 0...@ranks.length
-      @deck << Card.new(@ranks[i],@suits[0]) << Card.new(@ranks[i],@suits[1])
-      @deck << Card.new(@ranks[i],@suits[2]) << Card.new(@ranks[i],@suits[3])
-      end
+
+    if cards == nil
+      @cards = generate_deck
     else
-      @deck = deck
+      @cards = cards
     end
-  end
-
-  def each
-    for i in 0...size
-      yield @deck[i]
-    end
-  end
-
-  class Hand
-    def initialize(hand)
-      @data = hand
-    end
-
-    def size
-      @data.length
-    end
-
-    def twenty?(trump_suit)
-      ! forty?(trump_suit) and
-       (forty?(:spades) or forty?(:hearts) or
-         forty?(:diamonds) or forty?(:clubs))
-    end
-
-    def forty?(trump_suit)
-      @data.include?(Card.new(:queen, trump_suit)) and
-      @data.include?(Card.new(:king, trump_suit))
-    end
-
   end
 
   def deal
-    shuffle
-    hand = []
-    6.times do
-      if @deck.length != 0
-        hand << draw_top_card
-      end
+    hand = SixtySixHand.new @cards[0...@hand_size]
+    @cards = @cards.drop @hand_size
+    hand
+  end
+
+  class SixtySixHand < Hand
+
+    def initialize(cards)
+      super
     end
-    Hand.new(hand)
+
+    def twenty?(trump_suit)
+      not forty?(trump_suit) and ((forty?(:spades) or forty?(:hearts) or
+          forty?(:diamonds) or forty?(:clubs)))
+    end
+
+    def forty?(trump_suit)
+      @cards.include?(Card.new(:queen, trump_suit)) and
+      @cards.include?(Card.new(:king, trump_suit))
+    end
+
   end
 
 end
+
+
+
