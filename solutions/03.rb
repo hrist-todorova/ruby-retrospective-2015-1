a6e364c4fe08d247f0c9c04f040fe34d4d73a594
hrist-todorova <hrist_todorova@abv.bg>
2016-01-26 13:34:55 +0200

    shorter and smarter functions

--- solutions/03.rb
+++ solutions/03.rb
@@ -1,66 +1,67 @@
+class Integer
+  def prime?
+    return false if self == 1
+    2.upto(self ** 0.5).all? { |n| self % n != 0 }
+  end
+end
+
 class RationalSequence
   include Enumerable
 
   def initialize(limit)
-    @limit = limit - 1
-    @saver = limit
-    @arr = []
-    @step = 2
+    @limit = limit
   end
 
-  def to_a
-    if @limit == - 1 then return @arr else @arr.push(Rational(1,1)) end
-    while(@limit > 0)
-      i = 1
-      while(@limit > 0 and i <= @step)
-        @arr.push(Rational(@step - i + 1, i)).uniq!
-        @limit = @saver - @arr.length
-        i += 1
+  def each
+    counter = 0
+    row, col = 1, 1
+    while counter < @limit
+      if row.gcd(col) == 1
+        yield Rational(row, col)
+        counter += 1
       end
-      @step += 1
-      i = 1
-      while(@limit > 0 and i <= @step)
-        @arr.push(Rational(i, @step - i + 1)).uniq!
-        @limit = @saver - @arr.length
-        i += 1
-      end
-      @step += 1
+      row, col = calculate_direction(row, col)
     end
-    @arr
   end
-end
 
+  def calculate_direction(row, col)
+    if col == 1 and row.odd?
+      row += 1
+    elsif row == 1 and col.even?
+      col += 1
+    elsif (row + col).even?
+      row += 1
+      col -= 1
+    else
+      row -= 1
+      col += 1
+    end
+      return row, col
+  end
+
+end
 
 class PrimeSequence
   include Enumerable
 
   def initialize(limit)
-  @limit = limit
+    @limit = limit
   end
 
   def each
     stop = 0
     current_number = 2
 
-    while stop < @limit do
-      if prime?(current_number)
+    while stop < @limit
+      if current_number.prime?
         yield current_number
-        stop = stop+1
-       end
-       current_number = current_number + 1
+        stop += 1
+      end
+      current_number += 1
     end
   end
 
-
-  def prime?(n)
-    return false if n <= 1
-    2.upto(n-1) do |x|
-      return false if n%x == 0
-                   end
-      true
-  end
 end
-
 
 class FibonacciSequence
   include Enumerable
@@ -72,15 +73,12 @@ class FibonacciSequence
   end
 
   def each
-    if(@limit == 0)
-    yield
-    return
-    end
+    return if @limit == 0
     stop = 0
 
     yield @first
-    while stop < @limit-1
-      stop = stop + 1
+    while stop < @limit - 1
+      stop += 1
       yield @second
       @first, @second = @second, @first + @second
     end
@@ -88,59 +86,39 @@ class FibonacciSequence
 
 end
 
-
 module DrunkenMathematician
   module_function
 
   def meaningless(n)
-    if (n == 0)
-      return 0
+    return 1 if n == 0
+    groups = RationalSequence.new(n).partition do |rational|
+      rational.numerator.prime? or rational.denominator.prime?
     end
-    arr = RationalSequence.new(n).to_a
-    first_array = []
-    second_array = []
-    for i in 0...n
-      if !PrimeSequence.new(1).prime?(arr[i].numerator) and
-         !PrimeSequence.new(1).prime?(arr[i].denominator)
-        second_array << arr[i]
-      else
-        first_array << arr[i]
-      end
-    end
-    product_one = 1
-    first_array.each { |a| product_one *= a }
-    product_two = 1
-    second_array.each { |a| product_two *= a }
-    product_one / product_two
+
+    groups[0].reduce(1, :*) / groups[1].reduce(1, :*)
   end
 
   def aimless(n)
-    if (n == 0)
-      return 0
+    return 0 if n == 0
+    numbers = PrimeSequence.new(n).each_slice(2).map do |pair|
+      pair.length == 2 ? Rational(pair.first, pair.last) :
+                         Rational(pair.first, 1)
     end
-    sum = 0
-    arr = PrimeSequence.new(n).to_a
-    (0..n/2).each do |i|
-        if i%2 == 0
-        sum = sum + Rational(arr[i],arr[i+1])
-        end
-    end
-    sum
+    numbers.reduce(:+)
   end
 
   def worthless(n)
-    if (n == 0)
-      0
-    else
-      number = FibonacciSequence.new(n).to_a.at(n-1)
-      i = 1
-      sum = RationalSequence.new(i).to_a.reduce :+
-      while sum.ceil <= number
-       i += 1
-       sum = RationalSequence.new(i).to_a.reduce :+
-      end
-      RationalSequence.new(i-1).to_a
+    return [] if n == 0
+    fibonacci = FibonacciSequence.new(n).to_a.last
+
+    cut = 1
+    rationals_cut = RationalSequence.new(cut)
+    while rationals_cut.reduce(:+) <= fibonacci
+      cut += 1
+      rationals_cut = RationalSequence.new(cut)
     end
+
+    RationalSequence.new(cut - 1).to_a
   end
 
 end
